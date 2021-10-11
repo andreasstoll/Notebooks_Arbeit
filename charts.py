28ab612471da08b314101eb758a813371973ac2d
Andreas Stoll <andreasstoll@gmx.ch>
2021-10-11 15:42:03 +0200

    Diagramme

---
+++ charts.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+def create_quantity_series(table: pd.DataFrame, col: str)->pd.Series:
+    quantity_dict={}
+    for element in table[col].unique():
+        quantity_dict[element]= len(table[col].loc[table[col]==element]) 
+    quantity_series = pd.Series(quantity_dict)
+    quantity_series.name = col
+    return quantity_series
+
+def pie_chart(table: pd.DataFrame, col: str)->None:
+    pie_chart_series = create_quantity_series(table, col)
+    pie_chart_series.sort_index(inplace=True)
+    ax=pie_chart_series.plot(kind="pie", autopct='%1.1f%%', labels=None)
+    ax.legend(loc="upper right", bbox_to_anchor=(1.5, 1), labels=pie_chart_series.index)
+
+def bar_chart(table: pd.DataFrame, col: str)->None:
+    pie_chart_series = create_quantity_series(table, col)
+    ax=pie_chart_series.plot(kind="bar", rot=60)
